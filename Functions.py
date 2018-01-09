b7ca45e9d0ee10b6cf40ea54da533e525cc775fd
LeeHuangChen <lhc1@rice.edu>
2018-01-09 12:17:07 -0600

    added the updated template

--- Functions.py
+++ Functions.py
@@ -1,5 +1,35 @@
+import os
+import Configurations as conf
+import util
+from macros import *
+
+def printName(fileInfo):
+	inputFolder=fileInfo[1]
+	inputfile=fileInfo[2]
+	print inputfile
+
+def printLine(fileInfo,lineInfo):
+	lineIndex=lineInfo[0]
+	line=lineInfo[1]
+	print line
+
+def printAllLinesInFile(fileInfo):
+	forAllLineInFile(fileInfo,printLine)
+
+def printAllLinesInAllFiles():
+	forAllInputFiles(printAllLinesInFile,conf.inputFolder)
+
+
+def printAllNames():
+	forAllInputFiles(printName,conf.inputFolder)
+
+def test():
+	#printAllNames()
+	printAllLinesInAllFiles()
+	
+
 def main():
-	print 0
+	test()
 
 if __name__ == '__main__':
 	main()
